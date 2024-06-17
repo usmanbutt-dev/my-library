a8b11e221fb9c5a0fa89e873121bc41fb8766c9d
usmanbutt-dev <buttu7666@gmail.com>
2024-06-17 10:19:56 +0500

    Basic player functionality added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("working");
+let grid_cells = document.querySelectorAll(".grid-cell");
+const tick = `assets/tick.png`
+document.addEventListener("DOMContentLoaded",function(){
+
+
+    grid_cells.forEach(cell => {
+        cell.addEventListener("click", () => {
+            console.log ("ehe");
+            cell.classList.remove("grid-cell");
+            cell.classList.add("grid-cell-clicked");
+            cell.style.backgroundImage = `url("${tick}")`;
+            grid_cells = Array.from(grid_cells).filter(gridCell => gridCell !== cell);            
+        }, {once:true});
+    })
+
+})
